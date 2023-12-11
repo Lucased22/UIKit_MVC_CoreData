e47a69cb75e30ab6ed1df7b32132592c1cd2d23b
Lucased22 <lucaseduardo.siq@gmail.com>
2023-12-11 14:25:08 -0400

    Initial Commit

---
+++ UIKit_MVC_CoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit_MVC_CoreData
+//
+//  Created by Lucas Santos on 11/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
